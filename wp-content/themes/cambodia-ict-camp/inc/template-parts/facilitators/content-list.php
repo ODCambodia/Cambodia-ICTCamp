543ef7e048a579c4678c356e1084244b82a38ed8
Mardy <mardy@odc.org>
2018-11-13 17:53:45 +0700

    [TASK] Add linkedIn button in bio modal

--- wp-content/themes/cambodia-ict-camp/inc/template-parts/facilitators/content-list.php
+++ wp-content/themes/cambodia-ict-camp/inc/template-parts/facilitators/content-list.php
@@ -19,7 +19,7 @@
     <p class="text-center">
         <b><?php echo get_the_title(); ?></b>
         <br/>
-        <?php _e( $facilitator_expertise ); ?>
+        <em><?php _e( $facilitator_expertise ); ?></em>
         <br/>
         <?php _e( $facilitator_organization ); ?>
     </p>
@@ -41,12 +41,21 @@
                         <div class="col-xs-12 col-md-8">
                             <p>
                                 <b>
-                                    <a target="_blank" href="<?php echo $facilitator_linked_in_link; ?>">
-                                        <?php _e( get_the_title() ); ?>
-                                    </a>
+                                    <?php 
+                                    _e( get_the_title() );
+                                    
+                                    if ( $facilitator_linked_in_link ) {
+                                    ?>
+                                        <a style="margin-left: 0.5em" target="_blank"
+                                            href="<?php echo $facilitator_linked_in_link; ?>"
+                                            title="LinkedIn"
+                                        >
+                                            <i class="fa fa-linkedin-square fa-lg" aria-hidden="true"></i>
+                                        </a>
+                                    <?php } ?>
                                 </b>
                                 <br>
-                                <i><?php echo $facilitator_expertise; ?></i>
+                                <em><?php _e( $facilitator_expertise ); ?></em>
                                 <br/>
                                 <span><?php _e( $facilitator_organization ); ?></span>
                             </p>
@@ -56,6 +65,5 @@
                 </div>
             </div>
         </div>
-    </div>
-    <!-- END Bio Modal -->
+    </div><!-- END Bio Modal -->
 </div>
