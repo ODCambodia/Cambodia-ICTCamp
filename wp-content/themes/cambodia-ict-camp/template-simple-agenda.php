4cd41d21c5dbb07c5111959d6c4a297018fdb362
Mardy <mardy@odc.org>
2018-12-03 12:09:11 +0700

    [IMP] Clean spacing on Agenda Template

--- wp-content/themes/cambodia-ict-camp/template-simple-agenda.php
+++ wp-content/themes/cambodia-ict-camp/template-simple-agenda.php
@@ -10,124 +10,121 @@ global $event_star_customizer_all_values;
 $event_star_hide_front_page_header = $event_star_customizer_all_values['event-star-hide-front-page-header'];
 
 if(
-	( is_front_page() && 1 != $event_star_hide_front_page_header )
-	|| !is_front_page()
+    ( is_front_page() && 1 != $event_star_hide_front_page_header )
+    || !is_front_page()
 ) {
-	?>
-	<div class="wrapper inner-main-title">
-		<div class="container">
-			<header class="entry-header init-animate">
-				<?php
+    ?>
+    <div class="wrapper inner-main-title">
+        <div class="container">
+            <header class="entry-header init-animate">
+                <?php
                 the_title( '<h1 class="entry-title">', '</h1>' );
 
                 if( 1 == $event_star_customizer_all_values['event-star-show-breadcrumb'] ) {
-					event_star_breadcrumbs();
-				}
-				?>
-			</header><!-- .entry-header -->
-		</div>
-	</div>
+                    event_star_breadcrumbs();
+                }
+                ?>
+            </header><!-- .entry-header -->
+        </div>
+    </div>
 <?php
 }
 ?>
 
 <div id="content" class="site-content container clearfix">
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-			<?php
-			if( have_posts() ) {
-				while( have_posts() ) {
-					the_post();
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
+            <?php
+            if( have_posts() ) {
+                while( have_posts() ) {
+                    the_post();
 
-					the_content();
-				}
-			}
+                    the_content();
+                }
+            }
 
-			$taxonomy = 'camp_year';
-			$args = [
-				'taxonomy'   => $taxonomy,
-				'parent'     => 0,
-				'hide_empty' => false
-			];
+            $taxonomy = 'camp_year';
+            $args = [
+                'taxonomy'   => $taxonomy,
+                'parent'     => 0,
+                'hide_empty' => false
+            ];
+            $years = get_terms( $args );
 
-			$years = get_terms( $args );
+            foreach ( $years as $year ) {
+                if ( $year->name == date('Y') ) {
+                    $days = get_terms( $taxonomy, [ 'child_of' => $year->term_id ] );
+                    ?>
 
-			foreach ( $years as $year ) {
-				if ( $year->name == '2018' ) {
-					// $days = get_term_children( $year->term_id, 'camp_year' );
-					$days = get_terms( $taxonomy, [ 'child_of' => $year->term_id ] );
-					?>
+                    <ul class="nav nav-tabs nav-justified no-margin" role="tab-list">
+                        <?php
+                        $counter = 0;
+                        $class_active = 'active';
 
-					<ul class="nav nav-tabs nav-justified no-margin" role="tab-list">
-						<?php
-						$counter = 0;
-						$class_active = 'active';
+                        foreach ( $days as $day ) {
+                            if ( $day->count > 0 ) {
+                                if( $counter != 0 ) {
+                                    $class_active = '';
+                                } else {
+                                    $day_active_in = $day->slug;
+                                }
+                                ?>
 
-						foreach ( $days as $day ) {
-							if ( $day->count > 0 ) {
+                                <li class="<?php echo $class_active; ?>" role="presentation">
+                                    <a href="<?php echo get_site_url() . '#' . $day->slug; ?>" aria-controls="<?php echo $day->slug; ?>" role="tab" data-toggle="tab">
+                                        <?php _e( $day->name ); ?>
+                                    </a>
+                                </li>
+                                <?php
+                                $counter++;
+                            }
+                        }
+                        ?>
+                    </ul>
+                <?php
+                }
+            }
+            ?>
 
-								if( $counter != 0 ) {
-									$class_active = '';
-								} else {
-									$day_active_in = $day->slug;
-								}
-								?>
+            <div class="tab-content">
+                <?php
+                $args = [
+                    'post_type'    => 'sessions',
+                    'tax_query'    => [
+                            'taxonomy' => $taxonomy,
+                            'field'    => 'term_id',
+                            'terms'    => $day->term_id
+                        ]
+                    ];
 
-								<li class="<?php echo $class_active; ?>" role="presentation">
-									<a href="<?php echo get_site_url() . '#' . $day->slug; ?>" aria-controls="<?php echo $day->slug; ?>" role="tab" data-toggle="tab">
-										<?php _e( $day->name ); ?>
-									</a>
-								</li>
-								<?php
-								$counter++;
-							}
-						}
-						?>
-					</ul>
-				<?php
-				}
-			}
-			?>
+                $sessions = new WP_Query( $args );
 
-			<div class="tab-content">
-				<?php
-				$args = [
-					'post_type'    => 'sessions',
-					'tax_query'    => [
-						'taxonomy' => $taxonomy,
-						'field'    => 'term_id',
-						'terms'    => $day->term_id
-					]
-				];
+                while( $sessions->have_posts() ) {
+                    $sessions->the_post();
 
-				$sessions = new WP_Query( $args );
+                    $terms = wp_get_post_terms( $post->ID, $taxonomy );
 
-				while( $sessions->have_posts() ) {
-					$sessions->the_post();
+                    foreach( $terms as $term ) {
+                        if( $term->parent != 0 ) {
 
-					$terms = wp_get_post_terms( $post->ID, $taxonomy );
-
-					foreach( $terms as $term ) {
-						if( $term->parent != 0 ) {
-
-							if( strcmp($term->slug, $day_active_in) == 0 ) {
-								$class_active_in = 'active in';
-							} else {
-								$class_active_in = '';
-							}
-							?>
-							<div class="tab-pane fade <?php echo $class_active_in; ?>" id="<?php echo $term->slug; ?>">
-								<h3><?php _e( $term->name ); ?></h3>
-								<?php echo $post->post_content; ?>
-							</div>
-						<?php
-						}
-					}
-				}
-				?>
-			</div>
-		</main>
-	</div>
+                            if( strcmp($term->slug, $day_active_in) == 0 ) {
+                                $class_active_in = 'active in';
+                            } else {
+                                $class_active_in = '';
+                            }
+                            ?>
+                            <div role="tabpanel" class="tab-pane fade <?php echo $class_active_in; ?>" id="<?php echo $term->slug; ?>">
+                                <h3><?php _e( $term->name ); ?></h3>
+                                <?php echo $post->post_content; ?>
+                            </div>
+                        <?php
+                        }
+                    }
+                }
+                ?>
+            </div>
+        </main>
+    </div>
 </div>
 
 <?php get_footer();
