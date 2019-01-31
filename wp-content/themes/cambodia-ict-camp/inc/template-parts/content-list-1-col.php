5e8d97cf5603ecd553911ce9cdfae55c04e0641b
Huy Eng <Huyeng@users.noreply.github.com>
2019-01-31 09:26:44 +0700

    Show order number of post in template

--- wp-content/themes/cambodia-ict-camp/inc/template-parts/content-list-1-col.php
+++ wp-content/themes/cambodia-ict-camp/inc/template-parts/content-list-1-col.php
@@ -7,6 +7,7 @@ $link = isset($params["link"]) ? $params["link"] : get_permalink();
 $max_num_tags = isset($params["max_num_tags"]) ? $params["max_num_tags"] : null;
 $show_tags = isset($params["$show_tags"]) ? $params["$show_tags"] : null;
 $show_meta = isset($params["show_meta"]) ? $params["show_meta"] : true;
+$show_order_number = isset($params["show_order_number"]) ? $params["show_order_number"] : "";
 $show_excerpt = isset($params["show_excerpt"]) ? $params["show_excerpt"] : false;
 $order = isset($params["order"]) ? $params["order"] : "publish_date";
 $extra_classes = isset($params["extra_classes"]) ? $params["extra_classes"] : null;
@@ -19,7 +20,7 @@ $no_blog_image = '';
         <div class="entry-content <?php echo $no_blog_image; ?>">
             <div class="entry-header-title">
                 <?php the_title( sprintf( '<h4 class="entry-title">
-                  <a class="txtcolor-dgreen" href="%s" rel="bookmark"><i class="fa fa-download"></i> ', esc_url( $link ) ), '</a></h4>' ); ?>
+                  <a class="txtcolor-dgreen" href="%s" rel="bookmark"><i class="fa fa-download"></i> '. $show_order_number, esc_url( $link ) ), '</a></h4>' ); ?>
             </div>
             <?php if ($show_meta): //$show_tags  ?>
               <div class="posts-meta-div">
