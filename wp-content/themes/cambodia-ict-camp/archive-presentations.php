ccb02ccf14ed6e05f844fbc9b4684743b31c8640
Mardy <mardy@odc.org>
2019-02-07 15:57:59 +0700

    [IMP] Code cleaning on Custom Link Meta Box

--- wp-content/themes/cambodia-ict-camp/archive-presentations.php
+++ wp-content/themes/cambodia-ict-camp/archive-presentations.php
@@ -24,12 +24,13 @@ global $event_star_customizer_all_values;
             $args = [
                 'taxonomy' => 'category',
                 'orderby'  => 'slug',
-                'order'  => 'ASC',
+                'order'    => 'ASC',
             ];
 
             $presentation_terms = get_terms( $args );
             $count_post_in_posttype = 0;
-            if ( !empty( $presentation_terms ) && !is_wp_error( $presentation_terms ) ) {
+
+            if ( ! empty( $presentation_terms ) && ! is_wp_error( $presentation_terms ) ) {
                 foreach ( $presentation_terms as $presentation_term ) {
                     wp_reset_query();
 
@@ -54,30 +55,38 @@ global $event_star_customizer_all_values;
                             <div class="section-title-left">
                                 <h2><?php _e( $presentation_term->name ); ?></h2>
                             </div>
+
                             <div class="setcion-body">
                                 <?php
                                 $counter = 1;
                                 $wrap_count = 6;
+
                                 while ( $presentation_group->have_posts() ) {
                                     $presentation_group->the_post();
 
-                                    if ($counter%$wrap_count == 1 ) {
+                                    if ( $counter%$wrap_count == 1 ) {
                                         echo '<div class="no-margin">';
                                     }
+
                                     $count_post_in_posttype++;
                                     $order_number = "P" . $count_post_in_posttype . ". ";
                                     $source_link = get_post_meta( $post->ID, '_custom_link_value_key', true );
-                                    get_ictcamp_template('content-list-1-col',array(
-                                  					"post" => get_post(),
-                                  					"show_meta" => true,
-                                            "show_order_number" => $order_number,
-                                  					"show_excerpt" => false,
-                                            "custom_link" => $source_link,
-                                            "no_custom_link_get_permalink" => false
-                                  			),true);
-                                  //  get_template_part( 'inc/template-parts/content', 'list-1-col' );
+                                    
+                                    get_ictcamp_template(
+                                        'content-list-1-col',
+                                        array(
+                                  			'post'                         => get_post(),
+                                  			'show_meta'                    => true,
+                                            'show_order_number'            => $order_number,
+                                  			'show_excerpt'                 => false,
+                                            'custom_link'                  => $source_link,
+                                            'no_custom_link_get_permalink' => false
+                                  		),
+                                        true
+                                    );
+                                    //get_template_part( 'inc/template-parts/content', 'list-1-col' );
 
-                                    if( ($counter%$wrap_count == 0) || ($counter == $presentation_group->post_count) ) {
+                                    if( ( $counter%$wrap_count == 0 ) || ( $counter == $presentation_group->post_count ) ) {
                                         echo '</div>';
                                     }
 
